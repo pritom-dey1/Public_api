093a4997c1446f0e6f04b83850238d949bf07344
pritom-dey1 <Pritom1.2.zx@gmail.com>
2025-10-27 01:51:50 +0600

    update views

--- apis/urls.py
+++ apis/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import (
     UserListAPI, UserDetailAPI,
-    ProductListCreateAPI, ProductDetailAPI
+    ProductListAPI, ProductDetailAPI
 )
 
 urlpatterns = [
     path('users/', UserListAPI.as_view(), name='users-list'),
     path('users/<int:pk>/', UserDetailAPI.as_view(), name='user-detail'),
 
-    path('products/', ProductListCreateAPI.as_view(), name='products-list-create'),
+    path('products/', ProductListAPI.as_view(), name='products-list'),
     path('products/<int:pk>/', ProductDetailAPI.as_view(), name='product-detail'),
 ]
